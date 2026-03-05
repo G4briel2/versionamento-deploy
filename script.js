576c06056cb081d03c8c52c77acb8d6285ef0410
Gabriel de Almeida Martins <gabriel_241@outlook.com.br>
2026-03-05 00:01:45 -0300

    Alpha 1.4.0 - Finalizado formulário de login

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-function redirect() {
-    setTimeout(() => {
-        window.location.href = './login/login.html';
-    }, 2000);
-}
+// function redirect() {
+//     setTimeout(() => {
+//         window.location.href = './login/login.html';
+//     }, 2000);
+// }
 
 // redirect();
+
+const inputSenha = document.getElementById('password');
+const btnShow = document.querySelector('.show');
+
+btnShow.addEventListener('click', () => {
+    const type = inputSenha.getAttribute('type') === 'password' ? 'text' : 'password';
+    inputSenha.setAttribute('type', type);
+    
+    btnShow.classList.toggle('visible');
+});
